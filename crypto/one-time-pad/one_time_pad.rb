6a4e6b68ce62f09577e6882fdaa7e4f119eb21cf
Alemão <rafael.rost@hp.com>
2016-05-05 21:22:48 -0300

    Finishing the coursework

--- crypto/one-time-pad/one_time_pad.rb
+++ crypto/one-time-pad/one_time_pad.rb
@@ -1,4 +1,5 @@
-cipher_text = ['3939252352554c5f51592621294d5c5229382f5d454b485d4554413132275458482d3157415046495c5b2a435a46543527364d5059394847382a2b4b555746404a38202d4c525652455c2a',
+cipher_text = [
+    '3939252352554c5f51592621294d5c5229382f5d454b485d4554413132275458482d3157415046495c5b2a435a46543527364d5059394847382a2b4b555746404a38202d4c525652455c2a',
     '4d514c503134405f305f4e44292c41534a57414f2c2c2d4054544d5f552741424c5931345f415d2c2e4d4454405e504142522e31424a434c5f2a2b415a412f585a55452d2e20394941562a',
     '56574023455d4449305b4745295b5b5a45385f59435a44574526453132445c5a454843404d585a2c4146464e3246544146554531445c49574a435f573a',
     '505725575951292c53594f515d435544484847522c2c4e5f4155575441274c45582d465d444c2e404b495859324f4f4227424131545644444d594e2e554a4b2c4757204947465f4c53572a',
@@ -9,7 +10,10 @@ cipher_text = ['3939252352554c5f51592621294d5c5229382f5d454b485d4554413132275458
     '5c6f607168346a607f7e6221616d613668387c62607a6861206a6d7f7b697224',
     '5c6f607168346a607f7e6221616d613668387c62607a6861206a6d7f7b697224']
 
+# Break the ciphers in pairs
+#
 ciphers = []
+
 cipher_text.each do |cipher|
   decoded = []
   cipher.scan(/../).each do |c|
@@ -18,118 +22,153 @@ cipher_text.each do |cipher|
   ciphers.push(decoded.join)
 end
 
-# OTP re-use adversary
-# if an ASCII character was XOR'd with a space it will be a case-swapped ASCII character
-def isxorspace(c)
-	return ((c >= 'a' and c <= 'z') || (c >= 'A' and c <= 'Z') || c.to_s == "\u0000")
+# Return true if is a space or a letter(a..zA..Z)
+#
+def space_or_letter(char)
+	(char =~ /[A-Za-z]/ || char.to_s == "\u0000")
 end
 
-def sxor(s1, s2)
-  out= ''
-  s1.size.times { |x| out += (s1[x].ord ^ s2[x].ord).chr }
+# Xor two strings
+#
+def xor(message1, message2)
+  xored = ''
+  message1.size.times { |x| xored += (message1[x].ord ^ message2[x].ord).chr }
+	xored
+end
+
+# normalize xored text
+#
+def normalize(text)
+	return '_' unless text
+	out = ''
+	text.size.times do |x|
+    out += (text[x] >= ' ' && text[x] <= '~') ? text[x] : '~'
+  end
 	out
 end
 
-def readable(data)
-	return '_' unless data
-	out= ''
-	data.size.times do |x|
-    out += (data[x] >= ' ' && data[x] <= '~') ? data[x] : '~'
-  end
-	return out
-end
-
-def print_plaintexts(key, ciphers)
-	puts ''
-	puts 'key:'
-  out = ''
+# Given a key and cipher texts, will print the plaintexts
+#
+def plaintexts(key, ciphers)
+  key_string = ''
   key.size.times do |x|
     if key[x].nil?
-      out += '__'
+      key_string += '__'
     else
-      out += '%02X' % key[x].ord
+      key_string += '%02X' % key[x].ord
     end
   end
 
-  puts "#{out}"
-  puts
-	puts
-	puts 'plaintexts:'
-
+  puts "Key: #{key_string}" 
+  
   ciphers.size.times do |y|
-    out= ''
+    texts = ''
     ciphers[y].size.times do |x|
       if key[x].nil?
-        out += '_'
+        texts += '_'
       else
-        out += readable(sxor(ciphers[y][x],key[x]))
+        texts += normalize(xor(ciphers[y][x],key[x]))
       end
     end
-    puts "MSG #{y}:"
-    puts "#{out}"
-    puts ''
+    puts "Plaintext #{y}:#{texts}"
   end
 end
 
-# auto crack re-used one time pad by spotting where messages were XOR'd with a space
-def otp_crack(candidates)
-	key = Array.new(candidates.max_by(&:size).size)
+def otp_attack(messages)
+	# Create an array with the max cipher size
+  #
+  key = Array.new(messages.max_by(&:size).size)
 
-  candidates.size.times do |r|
-		other = 1
-		mastermask= []
-    candidates[0].size.times { |x| mastermask.push(' ')}
-
-		# process the first ciphertext against all other candidates
-		while other < candidates.size
-			string1= candidates[0]
-			string2= candidates[other]
-			if string1.size > string2.size
-				maxlen= string2.size
+  # Iterate through all the messages rotating and comparing all of them
+  #
+  messages.size.times do |r|
+		message_to_compare = 1
+		spaces_on_message1 = []
+    messages[0].size.times { |x| spaces_on_message1.push(' ')}
+		
+		while message_to_compare < messages.size
+			message1 = messages[0]
+			message2 = messages[message_to_compare]
+			
+      # Use the min size between the messages being compared
+      #
+      if message1.size > message2.size
+				maxlen = message2.size
 			else
-				maxlen= string1.size
-      end
-			string1 = string1[0..maxlen - 1]
-			string2 = string2[0..maxlen - 1]
-			# we nullify the key by xoring the two messages together: result is m1 xor m2
-			m1m2= sxor(string1,string2)
-			# if we get an ASCII letter then one of the strings was an XOR with ' ' (0x20) (or some number or punctuation, so we will get some false positives)
-			# if we get an ASCII 0x00 then both of the strings were the same, and *may* have been a ' '
-			# (this also results in some false positives, but removes far more false negatives so is overall worth doing)
-			mask = []
-			m1m2.size.times do |x|
-				if isxorspace(m1m2[x])
-					mask.push(' ')
+				maxlen = message1.size
+      end			
+      message1 = message1[0..maxlen - 1]
+			message2 = message2[0..maxlen - 1]
+			
+      # Xor message 1 with message2
+			xored = xor(message1,message2)
+			
+      # Check for empty spaces in the xor result
+			xor_empty_spaces = []
+			xored.size.times do |x|
+				if space_or_letter(xored[x])
+					xor_empty_spaces.push(' ')
 				else
-					mask.push(nil)
+					xor_empty_spaces.push(nil)
         end
       end
 
-			# if we see a space in the same place for every message then we know it was in message 1, since that was the only constant
-			# and as we are going to repeat this process for every message, we should get every key byte that corresponds to a space in any message
-			mask.size.times do |x|
-				if mastermask[x] != mask[x]
-					mastermask[x]= nil
+			# If an spaces was already found at the same place
+      # we can guess that the space belongs to message1
+			xor_empty_spaces.size.times do |x|
+				if spaces_on_message1[x] != xor_empty_spaces[x]
+					spaces_on_message1[x]= nil
         end
       end
-			other += 1
+
+      # compares message1 with the next message
+			message_to_compare += 1
     end
 
-    # now we know where the spaces were in string1, an XOR with a space will reveal the key byte
-    string1.size.times do |x|
-      if mastermask[x] == ' '
-        key[x] = (string1[x].ord ^ ' '.ord).chr
+    # Having all the spaces of message1, we xor them to get the key
+    message1.size.times do |x|
+      if spaces_on_message1[x] == ' '
+        key[x] = (message1[x].ord ^ ' '.ord).chr
       end
     end
 		# rotate list
-		candidates = [candidates.pop] + candidates
+		messages = [messages.pop] + messages
   end
 	return key
 end
 
-key = otp_crack(ciphers)
+# Get the partial key
+#
+key = otp_attack(ciphers)
 
-# adjust the key mannually HERE
-#key[x] = ''
+# Print partial plain texts
+plaintexts(key, ciphers)
 
-print_plaintexts(key, ciphers)
+# Guess phase
+# 
+key[9] = (ciphers[7][9].ord ^ 'U'.ord).chr
+key[14] = (ciphers[7][14].ord ^ 'H'.ord).chr
+key[19] = (ciphers[7][19].ord ^ 'E'.ord).chr
+key[31] = (ciphers[7][31].ord ^ 'T'.ord).chr
+key[41] = (ciphers[7][41].ord ^ 'A'.ord).chr
+key[42] = (ciphers[7][42].ord ^ 'V'.ord).chr
+key[43] = (ciphers[7][43].ord ^ 'E'.ord).chr
+key[46] = (ciphers[7][46].ord ^ 'I'.ord).chr
+key[49] = (ciphers[7][49].ord ^ 'S'.ord).chr
+key[52] = (ciphers[7][52].ord ^ 'D'.ord).chr
+key[55] = (ciphers[7][55].ord ^ 'H'.ord).chr
+key[56] = (ciphers[7][56].ord ^ 'I'.ord).chr
+key[60] = (ciphers[7][60].ord ^ 'C'.ord).chr
+key[63] = (ciphers[7][63].ord ^ 'R'.ord).chr
+key[64] = (ciphers[7][64].ord ^ 'S'.ord).chr
+key[29] = (ciphers[0][29].ord ^ ' '.ord).chr
+key[70] = (ciphers[0][70].ord ^ 'O'.ord).chr
+key[71] = (ciphers[0][71].ord ^ 'K'.ord).chr
+key[72] = (ciphers[0][72].ord ^ 'E'.ord).chr
+key[73] = (ciphers[0][73].ord ^ 'N'.ord).chr
+key[74] = (ciphers[0][74].ord ^ '.'.ord).chr
+key[75] = (ciphers[6][75].ord ^ '.'.ord).chr
+
+# Prints plain texts and key
+#
+plaintexts(key, ciphers)
